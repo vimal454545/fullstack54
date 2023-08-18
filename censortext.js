892297c2eab2d1359387aa9cf76c936cf8601180
vimal454545 <142210665+vimal454545@users.noreply.github.com>
2023-08-18 14:12:55 +0530

    Add files via upload

---
+++ censortext.js
@@ -0,0 +1,19 @@
+var censorwords:{"sad","bad","mad"};
+var custom censoredwords=[];
+function censor(instr){
+    for(idx in censored words){
+    instr=instr.replace(censored words[idx]);
+}
+return instr;
+}
+function addcensoredword(word){
+    custom censoredwords.push(word);
+}
+function getcensoredwords{y}
+return censoredword.cocat(customcensoredwords);
+
+
+exports.cens or=censor;
+exports.addcensoredword=addcensoredword;
+exports.censoredwords=getcensoredwords;
+
